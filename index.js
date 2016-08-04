97f5a5e43ae0eec83b3dfbb35633403cd8cb469d
Lukas Eipert <git@leipert.io>
2016-08-04 14:39:10 +0200

    implement extraction of basic auth from `.npmrc` files
    Compare https://github.com/npm/npm/blob/v3.10.6/lib/config/get-credentials-by-uri.js#L51-L71

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var url = require('url')
 var tokenKey = ':_authToken'
+var userKey = ':username'
+var passwordKey = ':_password'
 
 module.exports = function (registryUrl, opts) {
   var options = opts || {}
@@ -7,9 +9,10 @@ module.exports = function (registryUrl, opts) {
   var parsed = url.parse(registryUrl || npmrc.registry, false, true)
 
   var match
+  var basicToken
   var pathname
 
-  while (!match && pathname !== '/') {
+  while (!match && !basicToken && pathname !== '/') {
     pathname = parsed.pathname || '/'
     var regUrl = '//' + parsed.host + pathname.replace(/\/$/, '')
 
@@ -25,6 +28,23 @@ module.exports = function (registryUrl, opts) {
       break
     }
 
+    var username = npmrc[regUrl + userKey] || npmrc[regUrl + '/' + userKey]
+    var password = npmrc[regUrl + passwordKey] || npmrc[regUrl + '/' + passwordKey]
+
+    if (username && password) {
+
+      // passwords are base64 encoded, so we need to decode it
+      // See https://github.com/npm/npm/blob/v3.10.6/lib/config/set-credentials-by-uri.js#L26
+      password = new Buffer(password, 'base64').toString('utf8')
+
+      // a basic auth token is base64 encoded 'username:password'
+      // See https://github.com/npm/npm/blob/v3.10.6/lib/config/get-credentials-by-uri.js#L70
+      basicToken = new Buffer(username + ':' + password).toString('base64')
+
+      // we found a basicToken token so let's exit the loop
+      break
+    }
+
     if (!options.recursive) {
       break
     }
@@ -36,6 +56,9 @@ module.exports = function (registryUrl, opts) {
     return {token: match, type: 'Bearer'}
   }
 
+  if (basicToken !== undefined) {
+    return {token: basicToken, type: 'Basic'}
+  }
 
   return undefined
 }
--- test/auth-token.test.js
+++ test/auth-token.test.js
@@ -9,6 +9,14 @@ var afterEach = mocha.afterEach
 var describe = mocha.describe
 var it = mocha.it
 
+function decodeBase64(base64) {
+  return new Buffer(base64, 'base64').toString()
+}
+
+function encodeBase64(string) {
+  return new Buffer(string, 'utf8').toString('base64')
+}
+
 /*eslint max-nested-callbacks: ["error", 4]*/
 
 describe('auth-token', function () {
@@ -149,6 +157,137 @@ describe('auth-token', function () {
         var getAuthToken = requireUncached('../index')
         assert(!err, err)
         assert.deepEqual(getAuthToken('https://registry.blah.com'), {token: 'whatev', type: 'Bearer'})
+        done()
+      })
+    })
+
+    it('should prefer bearer token over basic token', function (done) {
+      var content = [
+        'registry=http://registry.foobar.eu/',
+        'registry=http://registry.foobar.eu/',
+        '//registry.foobar.eu/:_authToken=bearerToken',
+        '//registry.foobar.eu/:_password=' + encodeBase64('foobar'),
+        '//registry.foobar.eu/:username=foobar', ''
+      ].join('\n')
+
+      fs.writeFile(npmRcPath, content, function (err) {
+        var getAuthToken = requireUncached('../index')
+        assert(!err, err)
+        assert.deepEqual(getAuthToken('//registry.foobar.eu'), {token: 'bearerToken', type: 'Bearer'})
+        done()
+      })
+    })
+
+  })
+
+  describe('basic token', function () {
+
+    it('should return undefined if password or username are missing', function (done, undef) {
+      var content = [
+        'registry=http://registry.foobar.eu/',
+        '//registry.foobar.eu/:_password=' + encodeBase64('foobar'),
+        '//registry.foobar.com/:username=foobar', ''
+      ].join('\n')
+
+      fs.writeFile(npmRcPath, content, function (err) {
+        var getAuthToken = requireUncached('../index')
+        assert(!err, err)
+        assert.equal(getAuthToken('//registry.foobar.eu'), undef)
+        assert.equal(getAuthToken('//registry.foobar.com'), undef)
+        done()
+      })
+    })
+
+    it('should return basic token if username and password are defined', function (done) {
+      var content = [
+        'registry=http://registry.foobar.eu/',
+        '//registry.foobar.eu/:_password=' + encodeBase64('foobar'),
+        '//registry.foobar.eu/:username=foobar', ''
+      ].join('\n')
+
+      fs.writeFile(npmRcPath, content, function (err) {
+        var getAuthToken = requireUncached('../index')
+        assert(!err, err)
+        var token = getAuthToken()
+        assert.deepEqual(token, {token: 'Zm9vYmFyOmZvb2Jhcg==', type: 'Basic'})
+        assert.equal(decodeBase64(token.token), 'foobar:foobar')
+        done()
+      })
+    })
+
+    it('should return basic token if registry url has port specified', function (done) {
+      var content = [
+        'registry=http://localhost:8770/',
+        // before the patch this token was selected.
+        '//localhost/:_authToken=ohno',
+        '//localhost:8770/:_password=' + encodeBase64('foobar'),
+        '//localhost:8770/:username=foobar', ''
+      ].join('\n')
+
+      fs.writeFile(npmRcPath, content, function (err) {
+        var getAuthToken = requireUncached('../index')
+        assert(!err, err)
+        var token = getAuthToken()
+        assert.deepEqual(token, {token: 'Zm9vYmFyOmZvb2Jhcg==', type: 'Basic'})
+        assert.equal(decodeBase64(token.token), 'foobar:foobar')
+        done()
+      })
+    })
+
+    it('should try with and without a slash at the end of registry url', function (done) {
+      var content = [
+        'registry=http://registry.foobar.eu',
+        '//registry.foobar.eu:_password=' + encodeBase64('barbay'),
+        '//registry.foobar.eu:username=barbaz', ''
+      ].join('\n')
+
+      fs.writeFile(npmRcPath, content, function (err) {
+        var getAuthToken = requireUncached('../index')
+        assert(!err, err)
+        var token = getAuthToken()
+        assert.deepEqual(token, {token: 'YmFyYmF6OmJhcmJheQ==', type: 'Basic'})
+        assert.equal(decodeBase64(token.token), 'barbaz:barbay')
+        done()
+      })
+    })
+
+    it('should fetch for the registry given (if defined)', function (done) {
+      var content = [
+        '//registry.foobar.eu:_authToken=barbaz',
+        '//registry.blah.foo:_password=' + encodeBase64('barbay'),
+        '//registry.blah.foo:username=barbaz',
+        '//registry.last.thing:_authToken=yep', ''
+      ].join('\n')
+
+      fs.writeFile(npmRcPath, content, function (err) {
+        var getAuthToken = requireUncached('../index')
+        assert(!err, err)
+        var token = getAuthToken('//registry.blah.foo')
+        assert.deepEqual(token, {token: 'YmFyYmF6OmJhcmJheQ==', type: 'Basic'})
+        assert.equal(decodeBase64(token.token), 'barbaz:barbay')
+        done()
+      })
+    })
+
+    it('recursively finds registries for deep url if option is set', function (done, undef) {
+      var opts = {recursive: true}
+      var content = [
+        '//registry.blah.com/foo:_password=' + encodeBase64('barbay'),
+        '//registry.blah.com/foo:username=barbaz',
+        '//registry.blah.eu:username=barbaz',
+        '//registry.blah.eu:_password=' + encodeBase64('foobaz'), ''
+      ].join('\n')
+
+      fs.writeFile(npmRcPath, content, function (err) {
+        var getAuthToken = requireUncached('../index')
+        assert(!err, err)
+        var token = getAuthToken('https://registry.blah.com/foo/bar/baz', opts)
+        assert.deepEqual(token, {token: 'YmFyYmF6OmJhcmJheQ==', type: 'Basic'})
+        assert.equal(decodeBase64(token.token), 'barbaz:barbay')
+        token = getAuthToken('https://registry.blah.eu/foo/bar/baz', opts)
+        assert.deepEqual(token, {token: 'YmFyYmF6OmZvb2Jheg==', type: 'Basic'})
+        assert.equal(decodeBase64(token.token), 'barbaz:foobaz')
+        assert.equal(getAuthToken('//some.registry', opts), undef)
         done()
       })
     })
